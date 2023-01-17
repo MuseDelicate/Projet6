54e97089289e23995c0d1408b1d9432de0af7b5c
Morgane Dubouays <dubouays.morgane@gmail.com>
2023-01-17 14:43:16 +0100

    route user fonctionnelle

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,6 @@ mongoose.connect('mongodb+srv://mused:mongo00Atlas@cluster0.afmut6c.mongodb.net/
 
 app.use(express.json());
 
-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -20,27 +19,6 @@ app.use((req, res, next) => {
     next();
 });
 
-
-app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
-});
-
-
-app.use('api/auth', userRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -14,6 +14,7 @@ exports.signup = (req, res, next) => {
                 .catch(error => res.status(400).json({ error }))
         })
         .catch(error => res.status(500).json({ error }));
+
 };
 
 exports.login = (req, res, next) => {
